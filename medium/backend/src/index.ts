b211d494cc39fa40b1002710fc08bc8ce1d293f7
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2024-03-10 14:18:36 +0530

    Added database url

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -2,8 +2,35 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.post('/api/v1/user/signup', (c) => {
   return c.text('Hello Hono!')
 })
 
-export default app
+
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+app.get('/api/v1/blog/:id', (c) => {
+  const id = c.req.param('id')
+	console.log(id);
+	return c.text('get blog route')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('Hello Hono!')
+})
+ 
+
+export default app;
