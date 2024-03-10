7d3c3768a2bee72d1b55baf7c61e26b767b38b34
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2024-03-10 14:54:11 +0530

    Added signup route

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,9 +1,34 @@
 import { Hono } from 'hono'
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { env } from 'process';
+
+
+dotenv.config();
+
+
+const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate())
 
 const app = new Hono()
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/user/signup', async (c) => {
+  const body = await c.req.json();
+  try{
+    const user = await prisma.user.create({
+      data:{
+        email : body.email,
+        password : body.password,
+      }
+    })
+    return c.text('jwt here');    
+  }
+  catch(e) {
+		return c.status(403);
+	}
+
 })
 
 
